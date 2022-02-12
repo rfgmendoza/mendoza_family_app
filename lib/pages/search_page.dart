50cc535faba30b6ab2854e2742edf46d4400e951
Rafael Mendoza <rfgmendoza@gmail.com>
2022-02-11 16:56:40 -0800

    add wrcode graphic

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -3,6 +3,7 @@ import 'package:mendoza_family_app/util/common_util.dart';
 import 'package:mendoza_family_app/util/common_widgets.dart';
 import 'package:mendoza_family_app/widgets/graph_renderer.dart';
 import 'package:mendoza_family_app/widgets/people_picker_page.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class SearchPage extends StatefulWidget {
   final FamilyPerson user;
@@ -36,102 +37,107 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget personCard(FamilyPerson? person) {
-    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 0, 64.0, 0),
-            child: Card(
-              color: Colors.lightBlue[200],
-              child: personTile(sourcePerson,
-                  trailing: sourcePerson.id != widget.user.id
-                      ? Column(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 0, 64.0, 0),
+          child: Card(
+            color: Colors.lightBlue[200],
+            child: personTile(sourcePerson,
+                trailing: sourcePerson.id != widget.user.id
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  sourcePerson = widget.user;
+                                });
+                              },
+                              icon: const Icon(Icons.person_off_rounded)),
+                        ],
+                      )
+                    : IconButton(
+                        onPressed: () {
+                          _openPeoplePicker(targetPerson?.id[0])
+                              .then((value) => {
+                                    if (value != null)
+                                      {
+                                        setState(() {
+                                          targetPerson = value as FamilyPerson;
+                                        })
+                                      }
+                                  });
+                        },
+                        icon: const Icon(Icons.person_search_rounded))),
+          ),
+        ),
+        const Divider(),
+        person != null
+            ? Column(
+                children: [
+                  Text(getRelationshipDescription(sourcePerson.id, person.id)),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(64.0, 0, 8.0, 0),
+                    child: Card(
+                      color: Colors.lightGreen[200],
+                      child: personTile(
+                        person,
+                        trailing: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             IconButton(
                                 onPressed: () {
-                                  setState(() {
-                                    sourcePerson = widget.user;
-                                  });
+                                  _openPeoplePicker(sourcePerson.id[0])
+                                      .then((value) => {
+                                            if (value != null)
+                                              {
+                                                setState(() {
+                                                  sourcePerson =
+                                                      value as FamilyPerson;
+                                                })
+                                              }
+                                          });
                                 },
-                                icon: const Icon(Icons.person_off_rounded)),
+                                icon: const Icon(Icons.person_search_rounded)),
                           ],
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            _openPeoplePicker(targetPerson?.id[0])
-                                .then((value) => {
-                                      if (value != null)
-                                        {
-                                          setState(() {
-                                            targetPerson =
-                                                value as FamilyPerson;
-                                          })
-                                        }
-                                    });
-                          },
-                          icon: const Icon(Icons.person_search_rounded))),
-            ),
-          ),
-          const Divider(),
-          person != null
-              ? Column(
-                  children: [
-                    Text(
-                        getRelationshipDescription(sourcePerson.id, person.id)),
-                    const Divider(),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(64.0, 0, 8.0, 0),
-                      child: Card(
-                        color: Colors.lightGreen[200],
-                        child: personTile(
-                          person,
-                          trailing: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
-                                  onPressed: () {
-                                    _openPeoplePicker(sourcePerson.id[0])
-                                        .then((value) => {
-                                              if (value != null)
-                                                {
-                                                  setState(() {
-                                                    sourcePerson =
-                                                        value as FamilyPerson;
-                                                  })
-                                                }
-                                            });
-                                  },
-                                  icon:
-                                      const Icon(Icons.person_search_rounded)),
-                            ],
-                          ),
                         ),
                       ),
-                    )
-                  ],
-                )
-              : Center(
-                  child: IconButton(
-                    icon: const Icon(Icons.person_add_alt_outlined),
-                    onPressed: () {
-                      _openPeoplePicker(sourcePerson.id[0])
-                          .then((value) => setState(() {
-                                targetPerson = value as FamilyPerson?;
-                              }));
-                    },
-                  ),
-                )
-        ],
-      )
-    ]);
+                    ),
+                  )
+                ],
+              )
+            : Center(
+                child: IconButton(
+                  icon: const Icon(Icons.person_add_alt_outlined),
+                  onPressed: () {
+                    _openPeoplePicker(sourcePerson.id[0])
+                        .then((value) => setState(() {
+                              targetPerson = value as FamilyPerson?;
+                            }));
+                  },
+                ),
+              )
+      ],
+    );
   }
 
   Widget searchOptions(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32.0),
+          child: Card(
+            child: QrImage(
+              data: widget.user.id,
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+          ),
+        ),
         personCard(targetPerson),
         ElevatedButton(
             onPressed: () {
